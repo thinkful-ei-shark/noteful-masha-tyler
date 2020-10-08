79f6b8101b1c7bb4fc5df2740e6e92e582be2b5f
Tyler Sharpee <sharpee94@gmail.com>
2020-10-08 14:08:33 -0400

    added key to mapping and corrected htmlFor

--- src/AddNoteMain/AddNoteMain.js
+++ src/AddNoteMain/AddNoteMain.js
@@ -11,25 +11,25 @@ export default class AddNote extends React.Component {
         <h2>Compose</h2>
         <form>
           <p>
-            <label for="note-title">Title</label>
+            <label htmlFor="note-title">Title</label>
           </p>
           <p>
             <input type="text" id="note-title" name="note-title" placeholder="Enter the note title" />
           </p>
           <p>
-            <label for="note-content">Content</label>
+            <label htmlFor="note-content">Content</label>
           </p>
           <p>
             <textarea id="note-content" name="note-content" placeholder="Enter content"></textarea>
           </p>
           <p>
-            <label for="folder">Folder</label>
+            <label htmlFor="folder">Folder</label>
           </p>
           <p>
             <select id="folder" name="folder">
               <option>Select Folder</option>
               {this.context.folders.map(folder => 
-                <option value={folder.id}>{folder.name}</option>
+                <option key={folder.id} value={folder.id}>{folder.name}</option>
               )}
             </select>
           </p>
