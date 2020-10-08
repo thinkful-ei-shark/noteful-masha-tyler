70a67a166bbb50327c3a9f1657b7446d8d7b0468
Marc Trius <derpayatz@gmail.com>
2020-10-08 16:16:28 -0500

    add propTypes to Note

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom'
 import { format } from 'date-fns'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -47,3 +48,8 @@ export default class Note extends React.Component {
   }
 }
 
+Note.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string,
+  onDeleteNote: PropTypes.func
+};
